ced874156498c9639246171398361fc34c16c508
Agnilonda <pagnilonda@gmail.com>
2023-10-03 18:04:28 +0000

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 for counter in range(97, 123):
-    if chr(counter) != 'q' and chr(counter) != 'e':
-        print("{}".format(chr(counter)), end="")
+    print("{}".format(chr(counter)), end="")
