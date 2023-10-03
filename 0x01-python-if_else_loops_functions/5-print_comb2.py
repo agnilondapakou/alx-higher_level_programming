3909e176c2f421735bc62dc6fdc1b3957e3179d7
Agnilonda <pagnilonda@gmail.com>
2023-10-03 20:32:10 +0000

    5. 00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(0, 100):
     if num == 99:
-        print(f"{num}")
+        print("{}".format(num))
     else:
-        print(f"{num:02}", end=", ")
+        print("{:02}".format(num), end=", ")
