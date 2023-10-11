928ef658fca32f4445b74a87eb33c1725f6ec727
Agnilonda <pagnilonda@gmail.com>
2023-10-11 22:57:10 +0000

    0. Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py/0-square_matrix_simple.py
@@ -3,4 +3,5 @@ def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
     for i in range(len(matrix)):
         new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
-    return new_matrix
+
+    return (new_matrix)
